1e7c4963c1fd15afa555dd2a6c23cdae977af3ff
gimelloc <gislainemcasa@gmail.com>
2025-02-04 08:13:21 -0300

    header, tabs and routes

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,17 @@
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="$gray100" fontSize="$sm">
+          Olá,
+        </Text>
+
+        <Heading color="$gray100" fontSize="$md">
+          Gislaine
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { HomeHeader } from "@components/HomeHeader";
+import { Center } from "@gluestack-ui/themed";
+
+export function Home() {
+  return (
+    <Center flex={1}>
+      <HomeHeader />
+    </Center>
+  );
+}
