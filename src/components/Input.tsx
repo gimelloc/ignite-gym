e8fa667932779a4b36c8b057a603a1806ce72fd7
gimelloc <gislainemcasa@gmail.com>
2025-02-08 14:55:39 -0300

    validação dos inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,28 +1,53 @@
-import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+  Input as GluestackInput,
+  InputField,
+} from "@gluestack-ui/themed";
 import { ComponentProps } from "react";
 
 type Props = ComponentProps<typeof InputField> & {
+  errorMessage?: string | null;
+  isInvalid?: boolean;
   isReadOnly?: boolean;
 };
 
-export function Input({ isReadOnly, ...rest }: Props) {
+export function Input({
+  isReadOnly = false,
+  errorMessage = null,
+  isInvalid = false,
+  ...rest
+}: Props) {
+  const invalid = !!errorMessage || isInvalid;
+
   return (
-    <GluestackInput
-      h="$14"
-      borderWidth="$0"
-      borderRadius="$md"
-      $focus={{ borderWidth: 1, borderColor: "$green500" }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        bg="$gray700"
-        px="$4"
-        color="$white"
-        fontFamily="$body"
-        placeholderTextColor="$gray300"
-        {...rest}
-      />
-    </GluestackInput>
+    <FormControl isInvalid={invalid} w="$full" mb="$4">
+      <GluestackInput
+        isInvalid={isInvalid}
+        h="$14"
+        borderWidth="$0"
+        borderRadius="$md"
+        $focus={{ borderWidth: 1, borderColor: invalid ?  "$red500" : "$green500" }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+        $invalid={{ borderWidth: 1, borderColor: "$red500" }}
+      >
+        <InputField
+          bg="$gray700"
+          px="$4"
+          color="$white"
+          fontFamily="$body"
+          placeholderTextColor="$gray300"
+          {...rest}
+        />
+      </GluestackInput>
+
+      <FormControlError>
+        <FormControlErrorText color="$red500">
+          {errorMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
